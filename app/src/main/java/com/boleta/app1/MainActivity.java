7493297b10a45587db5c2077fb08dbcbb767574d
hugoLara <hugoLara.h@gmail.com>
2020-02-21 15:12:48 -0300

    ADD:first cummit

--- app/src/main/java/com/boleta/app1/MainActivity.java
+++ app/src/main/java/com/boleta/app1/MainActivity.java
@@ -2,7 +2,14 @@ package com.boleta.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView miImagen = findViewById(R.id.miImagen);
+
+        Picasso.get().load("https://guide.fire-emblem-heroes.com/wp-content/uploads/myrrh_great_dragon.png(42 kB)\n").into(miImagen);
+
+
+
+
+        Button boton1 = findViewById(R.id.button01);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "este es mi toast",Toast.LENGTH_SHORT).show();
+
+                intentExample();
+            }
+
+        });
     }
+
+private void intentExample(){
+    Intent intent=new Intent(this, Main2Activity.class);
+    startActivity(intent);
+
+}
+
+
 }
